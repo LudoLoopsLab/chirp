6380fef275e90ff36f17fccc69193cc0194eb8d6
Ludovic Dumas <ludoloops@pm.me>
2023-06-08 23:15:31 -0400

    Posting loading state + Toast

--- src/server/api/posts.ts
+++ src/server/api/posts.ts
@@ -46,7 +46,7 @@ export const postRouter = createTRPCRouter({
   }),
   create: publicProcedure.input(
     z.object({
-      content: z.string().emoji().min(1).max(280)
+      content: z.string().emoji("Only emojis are allowed").min(1).max(280)
     })
   )
     .mutation(async ({ ctx, input }) => {
